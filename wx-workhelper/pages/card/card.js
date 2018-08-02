c1fe3094cf3aebda61bedd1a046c9a393e2c7a8e
caoxiangyu <c.9u@outlook.com>
2018-08-02 21:41:18 +0800

    提交打卡功能

--- wx-workhelper/pages/card/card.js
+++ wx-workhelper/pages/card/card.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 var app = getApp()
+var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
 
   data: {
@@ -13,7 +14,9 @@ Page({
       name: 'T.I.T 创意园',
       label: {
         content: '创意园'
-      }
+      },
+      toView: 'red',
+      scrollTop: 100
     }],
     circles: [
       {
@@ -23,7 +26,8 @@ Page({
         fillColor: "",
         radius: 500
       }
-    ]
+    ],
+    cardList: []
   },
   onLoad: function(option){
     this.mapCtx = wx.createMapContext('myMap')
@@ -32,4 +36,38 @@ Page({
   moveToLocation: function () {
     this.mapCtx.moveToLocation()
   },
+  enterCard: function(){
+    this.data.cardList.push({
+      title:"打卡成功",
+      label:"打卡时间",
+      desc:"打卡地点"
+    });
+    this.setData({
+      cardList: this.data.cardList
+    });
+  },
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  }
 })
