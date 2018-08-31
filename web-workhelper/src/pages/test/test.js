ca1db12625b702cb71ec0438a351595b7bb15be6
caoxiangyu <cxy@acmtc.com>
2018-08-31 18:30:27 +0800

    提交后台和web端框架

---
+++ web-workhelper/src/pages/test/test.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello world'
+  },
+  methods:{
+    show:function(){alert("run...")}
+  }
+})
---
+++ workhelper/helper-eureka/src/main/java/com/workhelper/eureka/EurekaServerApplication.java
@@ -0,0 +1,23 @@
+package com.workhelper.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: cxy@acmtc.com
+ * \* Date: 2018/8/30
+ * \* Time: 18:40
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
