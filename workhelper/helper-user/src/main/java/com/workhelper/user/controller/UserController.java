e113257be5cb8797ad772cdf8e3c4ff3ae23a211
caoxiangyu <357428589@qq.com>
2018-09-15 19:30:34 +0800

    提交单点刷新配置

--- workhelper/helper-user/src/main/java/com/workhelper/user/controller/UserController.java
+++ workhelper/helper-user/src/main/java/com/workhelper/user/controller/UserController.java
@@ -6,6 +6,8 @@ import com.workhelper.log.service.LogService;
 import com.workhelper.user.model.com.workhelper.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @Controller
+@RefreshScope
 public class UserController {
     @Autowired
     private UserDao userDao;
@@ -24,13 +27,15 @@ public class UserController {
     private UserService userService;
     @Autowired
     private LogService logService;
+    @Autowired
+    private Environment environment;
 
     @RequestMapping(value = "/hi")
     @ResponseBody
     public String hi()throws Exception{
-        System.out.println("user调用成功");
-        userService.user();
-        List<BaseUser> baseUser = userDao.find("from BaseUser where id=?","0eeab1c4056711e8b986525400b05776");
+        System.out.println("user调用成功" + environment.getProperty("test"));
+//        userService.user();
+//        List<BaseUser> baseUser = userDao.find("from BaseUser where id=?","0eeab1c4056711e8b986525400b05776");
 //        System.out.println(baseUser.get(0).getAccount());
 //        restTemplate.getForObject("http://helper-log/log",String.class);
 //        logService.log();
